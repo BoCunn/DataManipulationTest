a62af294cf6f2f4df86fcae171c3500f09c5f67e
BoCunn <bowen.cunningham@outlook.com>
2026-02-07 13:40:30 -0600

    added waveform generation and fixed invalid format of testAudio

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from IPython.display import Audio, display
 import pandas as pd
 from scipy.spatial.distance import euclidean
 
+
+
+
+# Load the audio file
+music, sr = librosa.load('data/testAudio.mp3', sr=None, mono=True,offset= 81, duration=60)
+
+# Extract features
+librosa.display.waveshow(music, sr=sr)
+plt.title('Waveform of the Audio')
+plt.xlabel('Time m:ss')
+plt.show()
