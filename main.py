7a65aac1b58bb8191ba2a31b140a47eed236a989
BoCunn <bowen.cunningham@outlook.com>
2026-02-06 19:13:41 -0600

    created data folder and added sample audio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
-from IPython.display import Audio
+from IPython.display import Audio, display
 import pandas as pd
 from scipy.spatial.distance import euclidean
+
